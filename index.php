f675eb2b1a99cb786c76d2901e8c1664717c2b45
frame88 <fleuzzi97@gmail.com>
2022-02-10 16:34:56 +0100

    consegna

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- Milestone 1
+A partire dall'array realizzato stamattina, realizziamo una API che chiameremo con Vue.
+Milestone 2
+Stampiamo in una pagina tutte le auto con Vue.
+Milestone 3
+Inseriamo un sistema di filtraggio con Vue su un paio di campi a scelta!
+Bonus
+Realizziamo una pagina php in cui stampiamo le auto e effettuiamo anche un filtro. -->
