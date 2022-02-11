2e13bd342456351f602eb6574d7b833fa4e9e8ad
frame88 <fleuzzi97@gmail.com>
2022-02-11 16:17:05 +0100

    base camp

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<!-- Milestone 1
-A partire dall'array realizzato stamattina, realizziamo una API che chiameremo con Vue.
-Milestone 2
-Stampiamo in una pagina tutte le auto con Vue.
-Milestone 3
-Inseriamo un sistema di filtraggio con Vue su un paio di campi a scelta!
-Bonus
-Realizziamo una pagina php in cui stampiamo le auto e effettuiamo anche un filtro. -->
+
+<?php
+// <!-- Milestone 1
+// A partire dall'array realizzato stamattina, realizziamo una API che chiameremo con Vue.
+// Milestone 2
+// Stampiamo in una pagina tutte le auto con Vue.
+// Milestone 3
+// Inseriamo un sistema di filtraggio con Vue su un paio di campi a scelta!
+// Bonus
+// Realizziamo una pagina php in cui stampiamo le auto e effettuiamo anche un filtro. -->
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-snack-b1</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+</head>
+<body>
+<div id="app">
+  
+</div>
+
+
+</body>
+</html
